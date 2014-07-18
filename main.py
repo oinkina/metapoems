c9d5dceb0e16da9bd648c23008f6ce2d7142b84d
Oinkina <lambdaoinks@gmail.com>
2014-07-18 13:43:49 -0400

    minor edits, mostly fixing indentations

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import webapp2
 jinja_environment = jinja2.Environment(loader=
   jinja2.FileSystemLoader(os.path.dirname(__file__)))
  
-class PoemHomeHandler(webapp2.RequestHandler):
+class PoemHandler(webapp2.RequestHandler):
   def get(self):
     template_values = {}
     template = jinja_environment.get_template('poem_home.html')
     self.response.out.write(template.render(template_values))
   
-app = webapp2.WSGIApplication([('/poem', PoemHomeHandler),
-  ],
-  debug=True)
+routes = [
+  ('/poem', PoemHandler),
+]
+app = webapp2.WSGIApplication(routes, debug=True)
