506d2a16eea0f44a0ae86374859610f3b07c2ccc
mikohime <audrissie@gmail.com>
2014-07-22 12:10:48 -0400

    tried to make a database?

--- corpdatastore.py
+++ corpdatastore.py
@@ -2,15 +2,21 @@ from google.appengine.ext import db
 
 
 class AuthorCorpus(db.Model):
-  author_ID = db.StringProperty()
+  author_ID = db.IntegerProperty()
   author_name = db.StringProperty()
-  corpus = db.StringProperty()
+  corpus = db.StringListProperty()
 
 
-acorp = AuthorCorpus(first_name='Antonio',
-                    last_name='Salieri')
+acorp = AuthorCorpus(author_ID=65,
+                    author_name="Shakespeare, William",
+                    corpus=[
+                    	"http://www.gutenberg.org/cache/epub/2264/pg2264.txt",
+                    	"http://www.gutenberg.org/cache/epub/2265/pg2265.txt",
+                    	"http://www.gutenberg.org/cache/epub/2267/pg2267.txt",
+               				]
 
-employee.hire_date = datetime.datetime.now().date()
-employee.attended_hr_training = True
+                    	)
 
-employee.put()
+acorp.put()
+
+
