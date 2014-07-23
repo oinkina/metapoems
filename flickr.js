d5ca81f206ee96e569fd5fee46b9d0a6a363b0eb
ecabrera96 <cssi2014@CSSI-MBP-25.local>
2014-07-23 15:06:58 -0400

    flickr js updated &  gen. poem page with buttons

--- flickr.js
+++ flickr.js
@@ -1,8 +1,6 @@
 var GetFlickrUrl = function(photo) {
-  return 'https://farm' + photo.farm +
-         '.staticflickr.com/' + photo.server +
-         '/' + photo.id + '_' + photo.secret + '.jpg';
-}
+  return 'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg';
+};
 
 var AddPhoto = function(photo) {
   $("#photos").append("<li><img src=\"" + GetFlickrUrl(photo) + "\"></li>");
@@ -19,16 +17,15 @@ var DisplayPhotos = function(flickrPhotos) {
 };
 
 var GetResults = function() {
-  console.log("Hey!");
+  console.log("Let's search for bugs or know how our page works!");
   var flickr = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=764a4955d5ea19e5e7ca0b4a8f9603a7&text=fractals&format=json&per_page=1&jsoncallback=?";
-  var params = { "tags": $("#searchbox").val()};
-  $("body").css( "background-image", "url(" + "https://farm3.staticflickr.com/2918/14719770844_436b32d57b.jpg" + ")" );
+  var params = { "tags": "fractals" };
+  $("body").css( "background-image", "url(" + GetFlickrUrl + ")" );
   $.getJSON(flickr, params, DisplayPhotos);
 };
 
 var Main = function() {
   GetResults();
-}
+};
 
 $(document).ready(Main); 
-  
