8c23da6fb6a7a99f696026ea45e5a0be27bbdb50
Oinkina <lambdaoinks@gmail.com>
2014-07-24 12:01:31 -0400

    deleted rhyme.py

--- rhyme.py
+++
@@ -1,6 +0,0 @@
-from google.appengine.api import urlfetch
-
-url = "http://rhymebrain.com/talk?function=getRhymes&word=hello"
-result = urlfetch.fetch(url)
-if result.status_code == 200:
-  print result.content
