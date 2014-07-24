565ea46b577ec3a22f405f82fb7aca018d44fc45
Oinkina <lambdaoinks@gmail.com>
2014-07-24 10:16:43 -0400

    rhyming broken because often not in corpus

--- markov.py
+++ markov.py
@@ -193,18 +193,26 @@ def main():
   except urllib2.URLError, e:
     print e
 
-  parsed = json.loads(result)
+  rhymesDicts = json.loads(result)
+
   scores = []
   words = []
-  for word in parsed:
+  for word in rhymesDicts:
     words.append(word["word"])
     scores.append(word["score"])
-  wordInCorpus = False
-  while not wordInCorpus:
-    chosenWord = words[sample(scores)] 
+
+  '''
+  Sample from rhyming words until one of them is in the corpus. 
+  Remove word each time it's picked and not in corpus
+  If you run out of words, pick the one with the highest frequency (first in rhymesDicts)
+  '''
+
+  word = None
+  while not word in corpus:
+    index = sample(scores)
+    chosenWord = words[index]
+    words.remove(words[index]) 
     print "Chosen Word: %s" %chosenWord
-    if chosenWord in corpus:
-      wordInCorpus = True
 
 
 
