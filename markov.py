b642136942d6dabd097440bce15b716ed99ecb4d
Oinkina <lambdaoinks@gmail.com>
2014-07-24 10:45:42 -0400

    deleted obsolete stuff in main.py

--- markov.py
+++ markov.py
@@ -4,7 +4,6 @@ from collections import Counter, defaultdict
 from random import randrange, choice, uniform
 import re
 
-import urllib2
 import json
 
 ### if there's no n-gram match during synthesis you try fewer-grams
@@ -185,36 +184,6 @@ def generatePoem(corpus):
 def main():
   corpus = 'corpus/alice_in_wonderland.txt'
   print generatePoem(corpus)
-
-
-  url = "http://rhymebrain.com/talk?function=getRhymes&word=my"
-  try:
-    result = urllib2.urlopen(url).read()
-  except urllib2.URLError, e:
-    print e
-
-  rhymesDicts = json.loads(result)
-
-  scores = []
-  words = []
-  for word in rhymesDicts:
-    words.append(word["word"])
-    scores.append(word["score"])
-
-  '''
-  Sample from rhyming words until one of them is in the corpus. 
-  Remove word each time it's picked and not in corpus
-  If you run out of words, pick the one with the highest frequency (first in rhymesDicts)
-  '''
-
-  word = None
-  while not word in corpus:
-    index = sample(scores)
-    chosenWord = words[index]
-    words.remove(words[index]) 
-    print "Chosen Word: %s" %chosenWord
-
-
 
 # def main():
 
